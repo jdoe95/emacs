5b1ba4425b42a436e4f94636aa322b2c9efbc5e9
John Doe <jdoe35087@gmail.com>
2020-08-31 12:57:42 -0400

    Add automatic uppercase, add regstruct skeleton

--- emacs-helper.el
+++ emacs-helper.el
@@ -8,14 +8,14 @@
 (define-skeleton regfield
   "Inserts register field macros."
   nil
-  '(setq periph-name (skeleton-read "Peripheral name: " periph-name))
-  '(setq reg-name   (skeleton-read "Register name: " reg-name))
-  '(setq reg-type   (skeleton-read "Register type: "reg-type))
+  '(setq periph-name (upcase (skeleton-read "Peripheral name: " periph-name)))
+  '(setq reg-name    (upcase (skeleton-read "Register name: " reg-name)))
+  '(setq reg-type    (skeleton-read "Register type: "reg-type))
   ("Insert field"
    nil
-    '(setq field-name (skeleton-read "Field name: "))
+    '(setq field-name (upcase (skeleton-read "Field name: ")))
     '(setq field-mask (skeleton-read "Field mask: " "1" ))
-    '(setq field-pos  (skeleton-read "Field pos: "))
+    '(setq field-pos  (upcase (skeleton-read "Field pos: ")))
     "#define " periph-name "_" reg-name "_" field-name "   "
     "((" reg-type ") (" field-mask " << " field-pos "))" \n)
   )
@@ -26,17 +26,33 @@
 (define-skeleton regval
   "Inserts register value macros."
   nil
-  '(setq periph-name (skeleton-read "Peripheral name: " periph-name))
-  '(setq reg-name   (skeleton-read "Register name: " reg-name))
-  '(setq reg-type   (skeleton-read "Register type: " reg-type))
-  '(setq field-name (skeleton-read "Field name: " field-name))
-  '(setq field-pos  (skeleton-read "Field pos: " field-pos))
+  '(setq periph-name (upcase (skeleton-read "Peripheral name: " periph-name)))
+  '(setq reg-name    (upcase (skeleton-read "Register name: " reg-name)))
+  '(setq reg-type    (skeleton-read "Register type: " reg-type))
+  '(setq field-name  (upcase (skeleton-read "Field name: " field-name)))
+  '(setq field-pos   (upcase (skeleton-read "Field pos: " field-pos)))
   ("Insert value"
    nil
-   '(setq field-val (skeleton-read "Field value: "))
-   '(setq val-name  (skeleton-read "Field value name: "))
+   '(setq field-val  (skeleton-read "Field value: "))
+   '(setq val-name   (upcase (skeleton-read "Field value name: ")))
    "#define " periph-name "_" reg-name "_" field-name "_" val-name "    "
    "((" reg-type ") (" field-val " << " field-pos "))" \n)
   )
   
   
+(define-skeleton regstruct
+  "Inserts register structs."
+  nil
+  '(setq periph-name (upcase (skeleton-read "Peripheral name: "
+					    periph-name)))
+  "#include <stdint.h>" \n
+  \n
+  "struct __attribute__((aligned, packed)) " (downcase periph-name)
+  "_struct" \n
+  > "{" > \n
+  > _ > \n
+  > "};" > \n
+  \n
+  "extern struct " (downcase periph-name) "_struct " periph-name ";"
+  \n
+  \n)
